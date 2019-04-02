721cb8fcea4713ad3bd3f8315da42e5490d8e058
Octavian <octavzz1999@gmail.com>
2019-04-02 19:24:24 +0300

    Jump to specific dates

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ let prevMonthButton = document.getElementById('prevMonth');
 let nextMonthButton = document.getElementById('nextMonth');
 let todayButton = document.getElementById('today');
 let title = document.getElementById('mytitle');
-let months =['January','February','March','April','May','June','July','August','September','October','November','December'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 let today = new Date();
 let currentMonthNumber = today.getMonth();
 let currentMonthLiteral = months[currentMonthNumber];
 let year = new Date().getFullYear();
-
+title.innerText = currentMonthLiteral + ' ' + year;
 
 function monthStartDate(month, year) {
   return new Date(year, month, 1);
 }
 
 function monthEndDate(month, year, dateOrNumberModifier) {
-  if(dateOrNumberModifier==true)
+  if (dateOrNumberModifier == true)
     return new Date(year, month, (32 - new Date(year, month, 32).getDate()));
   else
     return (32 - new Date(year, month, 32).getDate());
@@ -27,7 +27,7 @@ function drawDay(day, inCurrentMonth) {
   let grid = document.getElementById('day-grid');
   let dayNumberElement = document.createElement('li');
   dayNumberElement.className = 'day';
-  if(!inCurrentMonth)
+  if (!inCurrentMonth)
     dayNumberElement.classList.add('not-current-month');
   dayNumberElement.textContent = day;
   grid.appendChild(dayNumberElement);
@@ -36,7 +36,7 @@ function drawDay(day, inCurrentMonth) {
 
 function clearGrid() {
   let grid = document.getElementById('day-grid');
-  grid.innerHTML=' ';
+  grid.innerHTML = ' ';
 }
 
 function drawGrid(month, year) {
@@ -67,34 +67,32 @@ function drawGrid(month, year) {
 }
 
 prevMonthButton.addEventListener('click', () => {
-  let thisday = new Date(year,currentMonthNumber,24);
-  let prevMonthNumber = thisday.getMonth()-1;
+  let thisday = new Date(year, currentMonthNumber, 24);
+  let prevMonthNumber = thisday.getMonth() - 1;
   let prevMonthLiteral = months[prevMonthNumber];
-  currentMonthNumber=prevMonthNumber;
-  currentMonthLiteral=prevMonthLiteral;
-  if(currentMonthNumber===-1)
-      {
-          currentMonthNumber=11;
-          currentMonthLiteral=months[currentMonthNumber];
-          year--;
-      }
-  title.innerText=currentMonthLiteral + ' ' + year;
+  currentMonthNumber = prevMonthNumber;
+  currentMonthLiteral = prevMonthLiteral;
+  if (currentMonthNumber === -1) {
+    currentMonthNumber = 11;
+    currentMonthLiteral = months[currentMonthNumber];
+    year--;
+  }
+  title.innerText = currentMonthLiteral + ' ' + year;
   drawGrid(currentMonthNumber, year);
 })
 
 nextMonthButton.addEventListener('click', () => {
-  let thisday = new Date(year,currentMonthNumber,24);
-  let nextMonthNumber = thisday.getMonth()+1;
+  let thisday = new Date(year, currentMonthNumber, 24);
+  let nextMonthNumber = thisday.getMonth() + 1;
   let nextMonthLiteral = months[nextMonthNumber];
-  currentMonthNumber=nextMonthNumber;
-  currentMonthLiteral=nextMonthLiteral;
-  if(currentMonthNumber===12)
-      {
-          currentMonthNumber=0;
-          currentMonthLiteral=months[currentMonthNumber];
-          year++;
-      }
-  title.innerText=currentMonthLiteral + ' ' + year;
+  currentMonthNumber = nextMonthNumber;
+  currentMonthLiteral = nextMonthLiteral;
+  if (currentMonthNumber === 12) {
+    currentMonthNumber = 0;
+    currentMonthLiteral = months[currentMonthNumber];
+    year++;
+  }
+  title.innerText = currentMonthLiteral + ' ' + year;
   drawGrid(currentMonthNumber, year);
 })
 
@@ -102,7 +100,45 @@ todayButton.addEventListener('click', () => {
   currentMonthNumber = today.getMonth();
   currentMonthLiteral = months[currentMonthNumber];
   year = new Date().getFullYear();
-  title.innerText= currentMonthLiteral + ' ' + year;
+  title.innerText = currentMonthLiteral + ' ' + year;
   drawGrid(currentMonthNumber, year);
 })
+
 drawGrid(currentMonthNumber, year);
+
+// Select Specific Date Modal
+
+const modal = document.querySelector(".modal");
+const trigger = document.getElementById("selectDate");
+const closeButton = document.querySelector(".close-button");
+const continueButton = document.querySelector(".continue");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function selectSpecificDate() {
+  let monthToJump = document.getElementById('mDr').selectedIndex - 1;
+  let yearToJump = today.getFullYear() - document.getElementById('yDr').selectedIndex + 1;
+  drawGrid(monthToJump, yearToJump);
+  currentMonthNumber = monthToJump;
+  currentMonthLiteral = months[currentMonthNumber];
+  year = yearToJump;
+  title.innerText = currentMonthLiteral + ' ' + year;
+}
+
+function switchToSelectedDate() {
+  toggleModal();
+  selectSpecificDate();
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+continueButton.addEventListener("click", switchToSelectedDate);
+window.addEventListener("click", windowOnClick);
